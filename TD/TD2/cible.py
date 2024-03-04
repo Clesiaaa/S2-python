b3a16cedcba84883d7483b8abeea5f009b4d4dc5
Clesiaaa <szymek.folga@icloud.com>
2024-03-04 19:05:57 +0100

    maj

--- TD/TD2/cible.py
+++ TD/TD2/cible.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-n, x, i, w = 25, 0, 0
+n, x, i, w = 25, 0, 0, 10
 colors = ["blue", "green", "black", "yellow", "pink", "red"]
 
 root = tk.Tk()
@@ -8,14 +8,14 @@ root = tk.Tk()
 canvas = tk.Canvas(root, bg="black", width=500, height=500)
 canvas.pack()
 
-while n!=0:
+while n != 0:
     couleur = colors[i]
-    cercle=canvas.create_oval(499-x,499-x,100,100, fill=couleur, outline=couleur)
-    canvas.move(cercle, 0,0)
-    x = x + 10
-    n=n-1
+    cercle = canvas.create_oval(499 - x, 499 - x, 100 + x, 100 + x, fill=couleur, outline=couleur)
+    canvas.move(cercle, w, w)
+    x = x + w
+    n = n - 1
     i = i + 1
-    if i > 5:
+    if i >= len(colors):
         i = 0
-root.mainloop()
 
+root.mainloop()
